7c2b511ed62f66c4df126103e4cbf1b5f4be19ea
karima kandil <karima.kandil@um5r.ac.ma>
2022-10-15 16:50:58 -0700

    responsive

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function afficher(id) {
         pos++;
         cible.style.top = pos + 'px';
         cible.style.down = pos + 'px';
-    }//cible.style.display = 'block';
+    }cible.style.display = 'block';
    
     // window.setTimeout(slowShow(),500);
     // window.setTimeout(function(){
@@ -34,17 +34,34 @@ function afficher(id) {
 
 }
 
+//-----------exercice 2---------
 function getMyToolTipFunction (id) {
     var toolpips = document.getElementsByClassName('displayText');
     var popup = document.getElementById(id);
     for (var i = 0; i < toolpips.length; i++) {
         toolpips[i].style.display = "none";
     }
-    
     popup.style.display="block";
-
-    
 }
 
+const starTotal = 5;
+const ratings = {
+    skill1 : 2.8,
+    skill2 : 3.3,
+    skill3 : 1.9,
+    skill4 : 4.3,
+    skill5 : 4.74
+  }; 
+for(const rating in ratings) {  
+  // 2
+  const starPercentage = (ratings[rating] / starTotal) * 100;
+  // 3
+  const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
+  // 4
+  document.querySelector(`.${rating}.listStar`).style.width = starPercentageRounded; 
+}
+
+   
+    
 
 
