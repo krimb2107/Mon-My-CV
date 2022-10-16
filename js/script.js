cfe01e8e105785f6d97579f5381327719612b539
karima kandil <karima.kandil@um5r.ac.ma>
2022-10-16 03:35:08 -0700

    'description'

--- js/script.js
+++ js/script.js
@@ -3,35 +3,11 @@
 function afficher(id) {
     var details = document.getElementsByClassName('details');
     var cible = document.getElementById(id);
-    // var Hfinal = 900;
-    // var Hinitilae =0;
-    // var time;
 
     for (i = 0; i < details.length; i++) {
         details[i].style.display = "none";
     }
-    // Hinitilae+=20;
-    // cible.style.height=Hinitilae+ 'px';
-
-    // time = setTimeout(afficher,700)
-    // cible.style.display = "block";
-    var hauteur = null;
-    var pos = 0;
-    hauteur = setInterval(frame, 500);
-    
-    function frame() {
-        pos++;
-        cible.style.top = pos + 'px';
-        cible.style.down = pos + 'px';
-    }cible.style.display = 'block';
-   
-    // window.setTimeout(slowShow(),500);
-    // window.setTimeout(function(){
-    //     cible.style.display = 'block';
-    //     cible.style.maxHeight='15em';
-    //     },500); 
-
-
+    cible.style.display = 'block';
 }
 
 //-----------exercice 2---------
@@ -42,8 +18,10 @@ function getMyToolTipFunction (id) {
         toolpips[i].style.display = "none";
     }
     popup.style.display="block";
+    setTimeout(getMyToolTipFunction,2000)
 }
 
+//-----------exercice 3---------
 const starTotal = 5;
 const ratings = {
     skill1 : 2.8,
