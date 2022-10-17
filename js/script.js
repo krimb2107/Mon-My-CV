61045cf8ef7dd91f3a263a6eff9a57b9b8426fe3
karima kandil <karima.kandil@um5r.ac.ma>
2022-10-16 17:34:05 -0700

    cv modification 2

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-const ratings = {
-    skill1 : 4.5,
-    skill2 : 4.0,
-    skill3 : 3.,
-    skill4 : 3.8,
-    skill5 : 4.2
-  }; 
-  const starTotal = 5;
-  var list = document.querySelectorAll('.listStar');
-for(const rating in ratings) {  
-  // 2
-  const starPercentage = (ratings[rating] / starTotal) * 100;
-  // 3
-  const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
-  // 4
-  document.querySelector(`.${rating} .listStar`).style.width = starPercentageRounded; 
-
-}
 
 function afficher(id) {
     var details = document.getElementsByClassName('details');
@@ -44,26 +26,16 @@ const barCanvas = document.getElementById("barCanvas");
 const barChat= new Chart(barCanvas,{
     type:"bar",
     data:{
-        labels:["HMTL", "CSS", "SASS", "JS", "JQuery"],
+        labels:["Elasticsearch", "PowerBI", "Talend", "Docker", "Git"],
         datasets:[{
-            label: 'Compétences',
+            label: 'Outils',
             data:[95,80,60,85,90],
             backgroundColor:["#b1eaff"]
         }]
     }
 })
 
-document.addEventListener('onmouseover', function (e) {
 
-    element.nextElementSibling.style.left =
-        (e.pageX + element.nextElementSibling.clientWidth + 10 < document.body.clientWidth)
-            ? (e.pageX + 10 + "px")
-            : (document.body.clientWidth + 5 - element.nextElementSibling.clientWidth + "px");
-    element.nextElementSibling.style.top =
-        (e.pageY + element.nextElementSibling.clientHeight + 10 < document.body.clientHeight)
-            ? (e.pageY + 10 + "px")
-            : (document.body.clientHeight + 5 - element.nextElementSibling.clientHeight + "px");
-});
 
 
 
